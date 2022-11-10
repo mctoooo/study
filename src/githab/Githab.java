c35408011e14b5055648094fc73a97308ef48cf4
Nikolay <Mc_an@rambler.ru>
2022-11-10 19:18:03 +0700

    done

--- src/githab/Githab.java
+++ src/githab/Githab.java
@@ -13,8 +13,14 @@ public class Githab {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
     
 }
+
+int i=0
+
+
+
