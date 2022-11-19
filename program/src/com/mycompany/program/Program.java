a91c5120766aee72c84fcc65c252e0b3b8c5628b
Nikolay <mc_an@rambler.ru>
2022-11-19 15:22:31 +0700

    Функция произведения элементов массива

--- program/src/com/mycompany/program/Program.java
+++ program/src/com/mycompany/program/Program.java
@@ -1,35 +1,35 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+* Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+*/
 package com.mycompany.program;
-
+import java.util.Scanner;
 /**
- *
- * @author Дракоша Тоша
- */
-
+*
+* @author Дракоша Тоша
+*/
 
 public class Program {
-    public static void PrintArrayToConsole(int []Mx){
+public static void PrintArrayToConsole(int [][]Mx){
 if(Mx != null){
-for(int ix = 0;ix < Mx.length; ix++) //функция распечатки массива
-System.out.print(Mx[ix]+" ");
+for(int ix = 0;ix < Mx.length; ix++){ //функция распечатки массива
+System.out.println(" " );
+for(int jx=0;jx<Mx.length;jx++){
+System.out.print(Mx[ix][jx]+" ");
+}
+}
 }
 else{
 System.out.print("Array is empty");
 }
 }
-
-    public static void main(String[] args) {
-
-int []Mx =null;
-Mx = CreateArray(20, 1,30);
-
+public static void main(String[] args) {
+int [][]Mx =null;
+Mx = CreateArray(15, 1,30);
 PrintArrayToConsole(Mx);
 System.out.println();
-
+double rez = Composition(Mx);
+System.out.println("произведение элементов равно = " +rez);
 }
 /*
 Функция создания и заполнения одномерного массива случайными значениями с заданными диапазоном
@@ -37,15 +37,28 @@ int size - количество элементов массива
 int rangeA - верхняя граница диапазона
 int rangeB - нижняя граница диапазона
 */
-public static int[] CreateArray(int size, int rangeA,int rangeB){
-int[] arr = null;//Объявляем массив
-arr = new int [size];// Создаем массив размерности size
+public static int[][] CreateArray(int size, int rangeA,int rangeB){
+int[][] arr = null;//Объявляем массив
+arr = new int [size][size];// Создаем массив размерности size
 for(int ix=0;ix<size;ix++){
-    
-arr[ix] = rangeA + (int) (Math.random() * rangeB);// Записываем значение, полученное в результате вычисления выражения, в ячейку
+for(int jx=0;jx<size;jx++){
+
+
+
+arr[ix][jx] = rangeA + (int) (Math.random() * rangeB);// Записываем значение, полученное в результате вычисления выражения, в ячейку
+}
 }
 return arr;// Возвращаем массив(ссылку)
 }
-
-
+public static double Composition(int [][]Mx) // функция произведения элементов массива
+{
+double com = 1;
+int row = Mx.length;
+for(int i = 0;i<row;i++){
+for(int j = 0;j<row;j++){
+com = com*Mx[i][j];
+}
+}
+return com;
+}
 }
