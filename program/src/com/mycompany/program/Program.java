309f06a2adf869a12c252c5e6d2089ceebb74159
Nikolay <mc_an@rambler.ru>
2022-11-19 15:58:49 +0700

    Функция поиска элемента в массиве  Возвращаем массив из трех элементов,где нулевой если равен нулю то такого элемента нет.   первый и второй  это индексы найденного элемента

--- program/src/com/mycompany/program/Program.java
+++ program/src/com/mycompany/program/Program.java
@@ -1,76 +1,85 @@
 /*
-* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-* Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
-*/
-package com.mycompany.program1;
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.program2;
+
 import java.util.Scanner;
+
 /**
-*
-* @author Дракоша Тоша
-*/
-public class Program1 {
-public static void main(String[] args) {
-int []arr =null;
-int repM=0;
-arr = CreateArray(20, 1,30);
-PrintArrayToConsole(arr);
-repM=Repit(arr);
-System.out.println(" \n элемент, встречающийся максимальное количество раз : "+repM);
-}
-/*Функция создания и заполнения одномерного массива случайными значениями с заданными диапазоном
-int size - количество элементов массива
-int rangeA - верхняя граница диапазона
-int rangeB - нижняя граница диапазона
-*/
-public static int[] CreateArray(int size, int rangeA,int rangeB){
-int[] arr = null;//Объявляем массив
-arr = new int [size];// Создаем массив размерности size
-for(int ix=0;ix<size;ix++){
-arr[ix] = rangeA + (int) (Math.random() * rangeB);// Записываем значение, полученное в результате вычисления выражения, в ячейку
-}
-return arr;// Возвращаем массив(ссылку)
-}
-//функция распечатки массива
-public static void PrintArrayToConsole(int []Mx){
+ *
+ * @author Дракоша Тоша
+ */
+
+
+public class Program2 {
+    public static void PrintArrayToConsole(int [][]Mx){
 if(Mx != null){
-for(int ix = 0;ix < Mx.length; ix++){
-System.out.print(Mx[ix]+" ");
+for(int ix = 0;ix < Mx.length; ix++){ //функция распечатки массива
+System.out.println("   " );
+    for(int jx=0;jx<Mx.length;jx++){
+    System.out.print(Mx[ix][jx]+" ");
+}
 }
 }
 else{
 System.out.print("Array is empty");
 }
 }
-//Функция поиска элемента встречающегося максимальное количество раз
-public static int Repit(int []Mx){
-int []repit =new int [Mx.length-1];
-repit=Mx;
-int k=1;
-int i=0;
-int L=1;
-int n=1;
-int max=repit[0];
-for (i=1;i<repit.length;i++){// находим максимальное число повторов
-if(max<repit[i]){
-max=repit[i];
+
+    public static void main(String[] args) {
+Scanner myScnr = new Scanner(System.in);
+int [][]Mx =null;
+Mx = CreateArray(20, 1,30);
+int []Rez=new int[3];
+
+PrintArrayToConsole(Mx);
+System.out.println();
+System.out.println("введите целое число ");
+int x = myScnr.nextInt();
+
+FindX(Mx,x);
+if (Rez[0]==1){
+    System.out.println("Число "+x+" найденно в ячейке [ "+Rez[1]+ " ][ "+Rez[2]+" ]" );
+}
+else{
+    System.out.println("Число "+x+" не найденно ");
 }
 }
-int x=repit[0];
-do{
-for (i=n;i<repit.length;i++){
-if ((repit[n-1]==repit[i])&&(repit[i]<max+1)){
-k++;
-repit[i]=max+1;
+/*
+Функция создания и заполнения одномерного массива случайными значениями с заданными диапазоном
+int size - количество элементов массива
+int rangeA - верхняя граница диапазона
+int rangeB - нижняя граница диапазона
+*/
+public static int[][] CreateArray(int size, int rangeA,int rangeB){
+int[][] arr = null;//Объявляем массив
+arr = new int [size][size];// Создаем массив размерности size
+for(int ix=0;ix<size;ix++){
+    for(int jx=0;jx<size;jx++){
+        
+        
+    
+arr[ix][jx] = rangeA + (int) (Math.random() * rangeB);// Записываем значение, полученное в результате вычисления выражения, в ячейку
 }
 }
-if(L<k){
-L=k;
-x=repit[n-1];
+return arr;// Возвращаем массив(ссылку)
 }
-k=1;
-n++;
+public static  void FindX(int [][]arr,int Num){ //Функция поиска элемента в массиве
+if(arr!=null){ // Возвращаем массив из трех элементов,где нулевой если равен нулю то такого элемента нет.
+    // первый и второй  это индексы найденного элемента
+
+ int[]Rez={0,0,0};
+for (int i=0;i<arr[0].length;i++){
+    for (int j=0;j<arr[0].length;j++){
+if(arr[i][j]==Num){
+  Rez[0]=1;
+  Rez[1]=i;
+  Rez[2]=j;  
 }
-while(n<repit.length-1);
-return x;
+}
+}
+}
 }
 }
