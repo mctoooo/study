faaad1c25f38b20ebdc0667b57b87ee4e1e3b4e1
Nikolay <mc_an@rambler.ru>
2022-11-19 15:12:23 +0700

    функция сортировки методом прямой вставки

--- program/src/com/mycompany/program/Program.java
+++ program/src/com/mycompany/program/Program.java
@@ -10,57 +10,117 @@ import java.util.Scanner;
 */
 
 public class Program {
-public static void PrintArrayToConsole(int [][]Mx){
-if(Mx != null){
-for(int ix = 0;ix < Mx.length; ix++){ //функция распечатки массива
-System.out.println(" " );
-for(int jx=0;jx<Mx.length;jx++){
-System.out.print(Mx[ix][jx]+" ");
+public static void main(String[] args) {
+int []arr =null;
+int repM=0;
+arr = CreateArray(20, 1,30);
+PrintArrayToConsole(arr);
+repM=Repit(arr);
+System.out.println(" \n элемент, встречающийся максимальное количество раз : "+repM);
+arr=Insert(arr);
+System.out.println("Массив упорядочен");
+PrintArrayToConsole(arr);
+System.out.println();
 }
+/*Функция создания и заполнения одномерного массива случайными значениями с заданными диапазоном
+int size - количество элементов массива
+int rangeA - верхняя граница диапазона
+int rangeB - нижняя граница диапазона
+*/
+public static int[] CreateArray(int size, int rangeA,int rangeB){
+int[] arr = null;//Объявляем массив
+arr = new int [size];// Создаем массив размерности size
+for(int ix=0;ix<size;ix++){
+
+arr[ix] = rangeA + (int) (Math.random() * rangeB);// Записываем значение, полученное в результате вычисления выражения, в ячейку
+}
+
+return arr;// Возвращаем массив(ссылку)
+}
+
+//функция распечатки массива
+public static void PrintArrayToConsole(int []Mx){
+if(Mx != null){
+for(int ix = 0;ix < Mx.length; ix++){ 
+System.out.print(Mx[ix]+" ");
 }
 }
 else{
 System.out.print("Array is empty");
 }
 }
-public static void main(String[] args) {
-Scanner myScnr = new Scanner(System.in);
-int [][]Mx =null;
-Mx = CreateArray(20, 1,30);
-int rez = Summ(Mx);
-PrintArrayToConsole(Mx);
-System.out.println();
-System.out.println("Сумма равна = " + rez);
-}
-/*
-Функция создания и заполнения одномерного массива случайными значениями с заданными диапазоном
-int size - количество элементов массива
-int rangeA - верхняя граница диапазона
-int rangeB - нижняя граница диапазона
-*/
-public static int[][] CreateArray(int size, int rangeA,int rangeB){
-int[][] arr = null;//Объявляем массив
-arr = new int [size][size];// Создаем массив размерности size
-for(int ix=0;ix<size;ix++){
-for(int jx=0;jx<size;jx++){
 
+//Функция поиска элемента встречающегося максимальное количество раз
+public static int Repit(int []Mx){
+int []repit =new int [Mx.length-1];
+repit=Mx;
+int k=1;
+int i=0;
+int L=1;
+int n=1;
+int max=repit[0];
+for (i=1;i<repit.length;i++){// находим максимальное число
+    if(max<repit[i]){
+        max=repit[i];
+    }
+}
+int x=repit[0];
+do{    
+for (i=n;i<repit.length;i++){
+    if ((repit[n-1]==repit[i])&&(repit[i]<max+1)){
+        k++;
+        repit[i]=max+1;
+    }
+    }
+    if(L<k){
+        L=k;
+        x=repit[n-1];
+    }
+        k=1;
+        n++;
+    }
+while(n<repit.length-1);
+return x;
+}
 
-
-arr[ix][jx] = rangeA + (int) (Math.random() * rangeB);// Записываем значение, полученное в результате вычисления выражения, в ячейку
+// функция сортировки методом прямой вставки
+public static int[] Insert(int []arr){
+int j=0;  // переменная цикла
+int i=0;  // переменная цикла
+int m=0;   // переменная цикла
+int n=0;   //переменная чикла
+int k=0;   // индекс упорядоченного элемента
+int L=0;  //индекс минимального элемента не упорядоченного массива
+int x=0;   //счетчик цикла
+int z=0;   // переменная для хранения минимального элемента
+int size=0; // переменная для определения длинны массива
+size=arr.length;
+x=1;
+for(j=size-1;j>0;j--,x++){
+    L=k+1;    // индекс начала неупорядоченной зоны
+for(i=L+1; i<size ; i++){
+if(arr[L]>arr[i]){   //ищем минимальный элемент
+L=i;
 }
 }
-return arr;// Возвращаем массив(ссылку)
+z=arr[L];
+if(arr[k]>arr[L]){
+   for(m=L;m>k;m--){
+   arr[m]=arr[m-1]; 
+}                   //сравниваем минимальный элемент с упорядоченным 
+                    //и выбираем место вставки:перед упорядоченным, после чего сдвигаем
+                    // элементы от минимального до места вставки перед упорядоченным
+   arr[k]=z;
+                    //вставляем элемент
 }
-public static int Summ(int [][]Mx) // функция суммы элементов массива
-{
-int sum = 0;
-int row = Mx.length;
-int column = Mx[0].length;
-for(int i = 0;i<row;i++){
-for(int j = 0;j<row;j++){
-sum = sum + Mx[i][j];
+else{
+     for(n=L;n>k+1;n--){       //...место вставки после упорядоченного
+   arr[n]=arr[n-1];            //сдвигаем элементы от минимального до места вставки после упорядоченного
 }
+  arr[k+1]=z;
 }
-return sum;
+k=k+1;                      // расширяем упорядоченную зону
+}
+return arr;
 }
 }
