afc68c5d87d17689738eeb42db9714fc41df4de8
Nikolay <mc_an@rambler.ru>
2022-11-19 15:26:52 +0700

    Функция проверки типа массива.возврашает одно цифровое значение // 1:(" Массив упорядочен по возрастанию."); // 2:(" Массив упорядочен по убыванию."); // 3:(" цифры одинаковы."); // 4:(" Массив не упорядочен.");

--- program/src/com/mycompany/program/Program.java
+++ program/src/com/mycompany/program/Program.java
@@ -10,55 +10,93 @@ import java.util.Scanner;
 */
 
 public class Program {
-public static void PrintArrayToConsole(int [][]Mx){
-if(Mx != null){
-for(int ix = 0;ix < Mx.length; ix++){ //функция распечатки массива
-System.out.println(" " );
-for(int jx=0;jx<Mx.length;jx++){
-System.out.print(Mx[ix][jx]+" ");
+public static void main(String[] args) {
+int []arr =null;
+int type=0;
+int repM=0;
+arr = CreateArray(20, 1,30);
+PrintArrayToConsole(arr);
+type=Type(arr);
+switch(type){
+    case 1:
+        System.out.println("\n Массив упорядочен по возрастанию.");
+        break;
+    case 2:
+        System.out.println("\n Массив упорядочен по убыванию.");
+        break;
+    case 3:
+        System.out.println(" \n цифры одинаковы.");
+        break;
+    case 4:
+        System.out.println("\n Массив не упорядочен.");
+        break;
+}    
 }
+
+/*Функция создания и заполнения одномерного массива случайными значениями с заданными диапазоном
+int size - количество элементов массива
+int rangeA - верхняя граница диапазона
+int rangeB - нижняя граница диапазона
+*/
+public static int[] CreateArray(int size, int rangeA,int rangeB){
+int[] arr = null;//Объявляем массив
+arr = new int [size];// Создаем массив размерности size
+for(int ix=0;ix<size;ix++){
+
+arr[ix] = rangeA + (int) (Math.random() * rangeB);// Записываем значение, полученное в результате вычисления выражения, в ячейку
+}
+
+return arr;// Возвращаем массив(ссылку)
+}
+
+//функция распечатки массива
+public static void PrintArrayToConsole(int []Mx){
+if(Mx != null){
+for(int ix = 0;ix < Mx.length; ix++){ 
+System.out.print(Mx[ix]+" ");
 }
 }
 else{
 System.out.print("Array is empty");
 }
 }
-public static void main(String[] args) {
-int [][]Mx =null;
-Mx = CreateArray(15, 1,30);
-PrintArrayToConsole(Mx);
-System.out.println();
-double rez = Composition(Mx);
-System.out.println("произведение элементов равно = " +rez);
-}
-/*
-Функция создания и заполнения одномерного массива случайными значениями с заданными диапазоном
-int size - количество элементов массива
-int rangeA - верхняя граница диапазона
-int rangeB - нижняя граница диапазона
-*/
-public static int[][] CreateArray(int size, int rangeA,int rangeB){
-int[][] arr = null;//Объявляем массив
-arr = new int [size][size];// Создаем массив размерности size
-for(int ix=0;ix<size;ix++){
-for(int jx=0;jx<size;jx++){
 
 
+//Функция проверки типа массива.возврашает одно цифровое значение
+// 1:(" Массив упорядочен по возрастанию.");
+// 2:(" Массив упорядочен по убыванию.");
+// 3:(" цифры одинаковы.");
+// 4:(" Массив не упорядочен.");
 
-arr[ix][jx] = rangeA + (int) (Math.random() * rangeB);// Записываем значение, полученное в результате вычисления выражения, в ячейку
+public static int Type(int []arr){
+int type=0;
+int j=0;  // переменная цикла
+int size=0; // переменная для определения длинны массива
+boolean s=true,s1=false,t=false,t1=false;
+size=arr.length;
+for(j=size-1;j>1;j--){
+if(s==arr[j]>arr[j-1]){     //упорядочен по возрастанию 
+   s1=!false;
+}
+else if(s==arr[j]<arr[j-1]){  //упорядочен по убыванию
+   t1=!false;              
+}
+ else{  //Цифры одинаковы
+   t=!false;
 }
 }
-return arr;// Возвращаем массив(ссылку)
+if((s==s1)&&(s!=t1)&&(s!=t)){
+  type=1;
 }
-public static double Composition(int [][]Mx) // функция произведения элементов массива
-{
-double com = 1;
-int row = Mx.length;
-for(int i = 0;i<row;i++){
-for(int j = 0;j<row;j++){
-com = com*Mx[i][j];
+else if((s!=s1)&&(s==t1)){
+  type =2;
+    }
+else if ((s==t)&&(s!=s1)&&(s!=t1)){
+  type=3;
 }
+else if((s==s1)&&(s==t1)){
+  type=4;
 }
-return com;
+return type;
 }
 }
