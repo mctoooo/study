d0345acf52aa81b369f8f4e5e427a9d9a2d3aef0
Nikolay <mc_an@rambler.ru>
2022-11-19 15:30:12 +0700

    функция проверки простого числа

--- program/src/com/mycompany/program/Program.java
+++ program/src/com/mycompany/program/Program.java
@@ -3,100 +3,44 @@
 * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
 */
 package com.mycompany.program;
-import java.util.Scanner;
 /**
 *
 * @author Дракоша Тоша
 */
-
+import java.util.Scanner;
 public class Program {
 public static void main(String[] args) {
-int []arr =null;
-int type=0;
-int repM=0;
-arr = CreateArray(20, 1,30);
-PrintArrayToConsole(arr);
-type=Type(arr);
-switch(type){
-    case 1:
-        System.out.println("\n Массив упорядочен по возрастанию.");
-        break;
-    case 2:
-        System.out.println("\n Массив упорядочен по убыванию.");
-        break;
-    case 3:
-        System.out.println(" \n цифры одинаковы.");
-        break;
-    case 4:
-        System.out.println("\n Массив не упорядочен.");
-        break;
-}    
-}
 
-/*Функция создания и заполнения одномерного массива случайными значениями с заданными диапазоном
-int size - количество элементов массива
-int rangeA - верхняя граница диапазона
-int rangeB - нижняя граница диапазона
-*/
-public static int[] CreateArray(int size, int rangeA,int rangeB){
-int[] arr = null;//Объявляем массив
-arr = new int [size];// Создаем массив размерности size
-for(int ix=0;ix<size;ix++){
+int a=0;
 
-arr[ix] = rangeA + (int) (Math.random() * rangeB);// Записываем значение, полученное в результате вычисления выражения, в ячейку
-}
+try (Scanner in = new Scanner(System.in)) {
 
-return arr;// Возвращаем массив(ссылку)
-}
+System.out.println("Введите целое положительное число: ");
+a = in.nextInt();
 
-//функция распечатки массива
-public static void PrintArrayToConsole(int []Mx){
-if(Mx != null){
-for(int ix = 0;ix < Mx.length; ix++){ 
-System.out.print(Mx[ix]+" ");
+String p = prime(a);
+System.out.println("Ваше число "+p );
 }
+}
+public static String prime(int Num){// функция проверки простого числа
+if (Num < 0){
+String pr =" меньше ноля.";
+return pr;
+}
+int x=Num;
+int k=0;
+String pr;
+for (int i=1;(i<=x)&&(x>=1) ;i++){
+if(x%i==0){
+k++;
+}
+}
+if(k==2){
+pr = " простое.";
 }
 else{
-System.out.print("Array is empty");
+pr = " не простое.";
 }
-}
-
-
-//Функция проверки типа массива.возврашает одно цифровое значение
-// 1:(" Массив упорядочен по возрастанию.");
-// 2:(" Массив упорядочен по убыванию.");
-// 3:(" цифры одинаковы.");
-// 4:(" Массив не упорядочен.");
-
-public static int Type(int []arr){
-int type=0;
-int j=0;  // переменная цикла
-int size=0; // переменная для определения длинны массива
-boolean s=true,s1=false,t=false,t1=false;
-size=arr.length;
-for(j=size-1;j>1;j--){
-if(s==arr[j]>arr[j-1]){     //упорядочен по возрастанию 
-   s1=!false;
-}
-else if(s==arr[j]<arr[j-1]){  //упорядочен по убыванию
-   t1=!false;              
-}
- else{  //Цифры одинаковы
-   t=!false;
-}
-}
-if((s==s1)&&(s!=t1)&&(s!=t)){
-  type=1;
-}
-else if((s!=s1)&&(s==t1)){
-  type =2;
-    }
-else if ((s==t)&&(s!=s1)&&(s!=t1)){
-  type=3;
-}
-else if((s==s1)&&(s==t1)){
-  type=4;
-}
-return type;
+return pr;
 }
 }
