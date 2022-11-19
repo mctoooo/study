f5f91ecd307e956b90a0736985afdb30593dba96
Nikolay <mc_an@rambler.ru>
2022-11-19 15:51:41 +0700

    Функция поиска элемента встречающегося максимальное количество раз

--- program/src/com/mycompany/program/Program.java
+++ program/src/com/mycompany/program/Program.java
@@ -2,42 +2,75 @@
 * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
 */
-package com.mycompany.program;
+package com.mycompany.program1;
+import java.util.Scanner;
 /**
 *
 * @author Дракоша Тоша
 */
-import java.util.Scanner;
-public class Program {
+public class Program1 {
 public static void main(String[] args) {
-
-int a=0;
-try (Scanner in = new Scanner(System.in)) {
-System.out.println("Введите целое положительное число: ");
-a = in.nextInt();
-String p = prime(a);
-System.out.println("Ваше число "+p );
+int []arr =null;
+int repM=0;
+arr = CreateArray(20, 1,30);
+PrintArrayToConsole(arr);
+repM=Repit(arr);
+System.out.println(" \n элемент, встречающийся максимальное количество раз : "+repM);
 }
+/*Функция создания и заполнения одномерного массива случайными значениями с заданными диапазоном
+int size - количество элементов массива
+int rangeA - верхняя граница диапазона
+int rangeB - нижняя граница диапазона
+*/
+public static int[] CreateArray(int size, int rangeA,int rangeB){
+int[] arr = null;//Объявляем массив
+arr = new int [size];// Создаем массив размерности size
+for(int ix=0;ix<size;ix++){
+arr[ix] = rangeA + (int) (Math.random() * rangeB);// Записываем значение, полученное в результате вычисления выражения, в ячейку
 }
-public static String prime(int Num){// функция проверки простого числа
-if (Num < 0){
-String pr =" меньше ноля.";
-return pr;
+return arr;// Возвращаем массив(ссылку)
 }
-int x=Num;
-int k=0;
-String pr;
-for (int i=1;(i<=x)&&(x>=1) ;i++){
-if(x%i==0){
-k++;
+//функция распечатки массива
+public static void PrintArrayToConsole(int []Mx){
+if(Mx != null){
+for(int ix = 0;ix < Mx.length; ix++){
+System.out.print(Mx[ix]+" ");
 }
-}
-if(k==2){
-pr = " простое.";
 }
 else{
-pr = " не простое.";
+System.out.print("Array is empty");
 }
-return pr;
+}
+//Функция поиска элемента встречающегося максимальное количество раз
+public static int Repit(int []Mx){
+int []repit =new int [Mx.length-1];
+repit=Mx;
+int k=1;
+int i=0;
+int L=1;
+int n=1;
+int max=repit[0];
+for (i=1;i<repit.length;i++){// находим максимальное число
+if(max<repit[i]){
+max=repit[i];
+}
+}
+int x=repit[0];
+do{
+for (i=n;i<repit.length;i++){
+if ((repit[n-1]==repit[i])&&(repit[i]<max+1)){
+k++;
+repit[i]=max+1;
+}
+}
+if(L<k){
+L=k;
+x=repit[n-1];
+}
+k=1;
+n++;
+}
+while(n<repit.length-1);
+return x;
 }
 }
